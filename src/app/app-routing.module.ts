a3a1a81552794551d4f7bb99526ae9af726443f2
Nishanth <m.nishanth1397@gmail.com>
2019-03-12 16:25:01 +0530

    CrickSpotApp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ const routes: Routes = [
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'create-account', loadChildren: './pages/create-account/create-account.module#CreateAccountPageModule' },
   { path: 'aboutus', loadChildren: './pages/aboutus/aboutus.module#AboutusPageModule' },
+  { path: 'create-team', loadChildren: './pages/create-team/create-team.module#CreateTeamPageModule' },
+  { path: 'user-account', loadChildren: './pages/user-account/user-account.module#UserAccountPageModule' },
 ];
 
 @NgModule({
