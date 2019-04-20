bc0184f824d810f1380e525b5c0e18c15c339c73
Mahdi <mahdirefaideen@gmail.com>
2019-04-20 21:39:40 +0530

    sign out function added

--- src/app/pages/user-account/user-account.page.ts
+++ src/app/pages/user-account/user-account.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-account',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAccountPage implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
+  }
+
+  signOut() {
+    console.log('Redirected to </home> page');
+    this.router.navigate(['/home']);
+  }
+
+  viewPlayers() {
+    console.log('Yet in maintainance');
+    // this.router.navigate([]);
+  }
+
+  createTeam() {
+    console.log('Redirected to </create-team> page');
+    this.router.navigate(['/create-team']);
   }
 
 }
